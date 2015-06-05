1b812775454c5d4ea9c0e67beec8e0b94d8304f1
User1 Desktop <smooth.alg@gmail.com>
2015-06-05 13:26:07 -0700

    Deploying Apps Working

---
+++ roles/bigip_app/files/__snat_random_rule.tcl
@@ -0,0 +1,20 @@
+when RULE_INIT {
+    expr srand("[clock clicks]")
+    set static::TARGET_VIP "/Common/demo_iApp.app/demo_iApp_vs"
+}
+when CLIENT_ACCEPTED {
+
+    set a [expr int(223*rand())]
+    set b [expr int(255*rand())]
+    set c [expr int(255*rand())]
+    set d [expr int(255*rand())]
+
+    while { $a == 192 || $a == 172 || $a == 10 } {
+        #log local0. "changing first octet from $a"
+        set a [expr int(223*rand())]
+    }
+    #log local0. $a.$b.$c.$d
+    snat $a.$b.$c.$d
+
+    virtual $static::TARGET_VIP
+}
