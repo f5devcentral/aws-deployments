49801cb09d4fa0edf0d2890c127dd85bfe58934a
User1 Desktop <smooth.alg@gmail.com>
2015-08-05 14:36:36 -0700

    Example of simple cluster deployed with bigsuds instead

--- library/bigip_cluster_orig.py
+++ library/bigip_cluster_orig.py
@@ -5,14 +5,14 @@ DOCUMENTATION = '''
 module: bigip_cluster
 short_description: "Manages F5 BIG-IP Device Services Clusters"
 description:
-    - "Manages F5 BIG-IP Device Services Clusters"
+    - "Manages F5 BIG-IP Device Services Clusters via LBaaS Plugin"
 version_added: "X.X"
 author: Alex Applebaum
 notes:
     - "Requires BIG-IP software version >= 11"
     - "Best run as a local_action in your playbook"
 requirements:
-    - bigsuds 
+    - f5 openstack LBaaS library
 options:
     hostname:
         description:
@@ -166,7 +166,6 @@ EXAMPLES = '''
 	devices: [ lb1.mydomain.com, lb2.mydomain.com ]
 	devices_mgmt_ips: [ 10.0.0.201, 10.0.0.202 ]
 
-  # NOT IMPLEMENTED YET
   - name: Simple delete cluster
     bigip_cluster:
 	hostname: lb1.mydomain.com
@@ -186,7 +185,6 @@ EXAMPLES = '''
 	action: traffic_group
 	traffic_group_name: traffic-group-2
 
-  # NOT IMPLEMENTED YET
   - name: Delete a traffic group
     bigip_cluster:
 	hostname: lb1.mydomain.com
@@ -196,8 +194,7 @@ EXAMPLES = '''
 	action: traffic_group
 	traffic_group_name: traffic-group-2
 
-  # NOT IMPLEMENTED YET
-  - name: Create lots of traffic groups (current max = 127 ): NOT IMPLEMENTED YET
+  - name: Create lots of traffic groups (current max = 127 )
     bigip_cluster:
 	hostname: lb1.mydomain.com
 	username: admin
@@ -215,24 +212,29 @@ import json
 # Need to add absolute path to library so python can find it
 # https://github.com/ansible/ansible/issues/4083
 # ex. can't use pwd as ansible actually runs module in a tmp directory
-# /home/vagrant/.ansible/tmp/ansible-tmp-1438633710.7-121872048074041/bigip_cluster
-# hopefully this directory is unique
-# aws_deployments_path = os.path.abspath("/aws-deployments/library")
-# sys.path.append(aws_deployments_path)
+#/home/vagrant/.ansible/tmp/ansible-tmp-1438633710.7-121872048074041/bigip_cluster
+#hopefully this directory is unique
+aws_deployments_path = os.path.abspath("/aws-deployments/library")
+sys.path.append(aws_deployments_path)
 
-import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
-import bigsuds
-
+import f5
+from f5.bigip.bigip import BigIP
 
 def main():
 
+
+#     modulenames = set(sys.modules)&set(globals())
+#     allmodules = [sys.modules[name] for name in modulenames]
+#     #module.fail_json(str(allmodules))
+#     print allmodules
+
+    
     changed = False
 
-    # grab connection args from bigip.py util
+    #grab connection args from bigip.py util
     argument_spec = bigip_argument_spec()
     
-    # add cluster specific args
+    #add cluster specific args
     argument_spec.update(
         dict(
             state=dict(type='str', choices=['present', 'absent'], required=True),
@@ -291,167 +293,99 @@ def main():
 
 
     try:
-        # Initalize the bigsuds connection object to SEED node
-        bigip_obj = bigsuds.BIGIP(
-                    hostname = hostname,
-                    username = username,
-                    password = password,
-                    )
-    except Exception, e:
-           print e
 
-    try: 
+	bigip_obj = BigIP( hostname, username, password, timeout, address_isolation, strict_route_isolation )
+ 
+       #print json.dumps(dir(bigip_obj.device))
+
+
 	if action == 'device':
 	
-           existing_device_name = bigip_obj.Management.Device.get_local_device()
-	   # print json.dumps({"existing_device_name": existing_device_name,
-           #                 "device_name": device_name})
-	   if existing_device_name != ("/Common/" + device_name):
+	   existing_device_name = bigip_obj.device.get_device_name()
+#	   print json.dumps({"existing_device_name": existing_device_name,
+#                            "device_name": device_name})
+	   if existing_device_name != device_name:
                 # have to reset device trust to set the proper device-name
-                bigip_obj.Management.Trust.reset_all(
-                                        device_object_name = device_name,
-                                        keep_current_authority = 'true',
-                                        authority_cert = '',
-                                        authority_key = '',
-                                      )
-
+		bigip_obj.device.reset_trust(device_name)
 	        result = { "changed": True, "Resetting Device Trust" : True }
 		time.sleep(15)
 
-	   # Begin Setting HA Channel Properities
-          
-           # Set ConfigSync Address 
-           bigip_obj.Management.Device.set_configsync_address( devices = [device_name], addresses = [configsync_addr] )
-
-           # Set Failover Address
-	   fo_ints = [ failover_addr ]
-	   unicast_objs = []
-	   for i in range(len(fo_ints)):
-		unicast_obj = {
-				'source'    : { 'address' : fo_ints[i], 'port' : 1026 },
-				'effective' : { 'address' : fo_ints[i], 'port' : 1026 }
-			      }
-		unicast_objs.append(unicast_obj)
-
-	   bigip_obj.Management.Device.set_unicast_addresses(
-							devices =   [device_name],
-                                                        addresses = [unicast_objs]
-                                                    )
- 	  
-           # Set Mirror Addresses
+# 	   #Begin Setting HA Channel Properities
+           bigip_obj.device.set_failover_addrs(ip_addrs=[failover_addr])
+           bigip_obj.device.set_configsync_addr(ip_address=configsync_addr)
+#  	  
            if primary_mirror_addr:
-                bigip_obj.Management.Device.set_primary_mirror_address(
-                                                            devices = [device_name],
-                                                            addresses = [primary_mirror_addr]
-                                                        )
+                #print json.dumps({"primary_mirror_address": primary_mirror_addr })
+		bigip_obj.device.set_primary_mirror_addr(ip_address=primary_mirror_addr)
            if secondary_mirror_addr:
-                bigip_obj.Management.Device.set_secondary_mirror_address(
-                                                            devices = [device_name],
-                                                            addresses = [secondary_mirror_addr]
-                                                        )
+                #print json.dumps({"secondary_mirror_address": secondary_mirror_addr })
+		bigip_obj.device.set_secondary_mirror_addr(ip_address=secondary_mirror_addr)
 
-
-	   # Should do some extra validation here with some gets before returning true
 	   result = { "changed": True, "Device Object Configured" : True }
 
 	if action == 'cluster':
 	    if state == 'present' and len(devices):
-                peer_exists = False
-                device_group_exists = False
-
-		# Check to see if Device Trust Group exists / already contains a peer device
-                existing_peers = bigip_obj.Management.Device.get_list()
-                for device in existing_peers:
-                    if device == devices[1]:
-                         peer_exists = True
-                         break
+		# Check to see if Device Trust Group exists and has the peer
+		# Can add some more validation here to iterate through all devices
+		peer_exists = bigip_obj.cluster.peer_exists(name=devices[1])
 
 		if device_group_name == None:
 		    device_group_name = "my-sync-failover-group"
 
 		# Check to see if there's a sync failover group
-		existing_device_groups = bigip_obj.Management.DeviceGroup.get_list()
-                for group in existing_device_groups:
-		    if group == ("/Common/" + device_group_name):
-		         device_group_exists = True
-                         break
-
+		device_group_exists = bigip_obj.cluster.cluster_exists(device_group_name)
 
 		if not peer_exists:
 
 		    ### GO TO SEED DEVICE AND CREATE CLUSTER
-		    # Can potentially make which device is the seed an option.
-		    # For now will be first in the list
+		    #Can potentially make which device is the seed an option.
+		    #For now will be first in the list
+		    # Due to, Bug alias 479071 TG initial-placement influenced by sync-failover DG w/auto-sync enabled 
+                    # Either need to offline peers that you're adding 
+                    # or not enable auto-sync when first creating sync-failover-group
+
 		    for i in range(len(devices)-1):
 
-			    bigip_obj.Management.Trust.add_authority_device(
-							    address = devices_mgmt_ips[i + 1],
-							    username = username,
-							    password = password,
-							    device_object_name = devices[i + 1],
-							    browser_cert_serial_number = '',
-							    browser_cert_signature = '',
-							    browser_cert_sha1_fingerprint = '',
-							    browser_cert_md5_fingerprint = '',
-							  )
-
+# 			    print json.dumps({
+# 						"adding_device" : devices[i + 1],
+# 						"mgmt_ip" : devices_mgmt_ips[i + 1],
+# 						"username" : username 
+# 					     })
+                            
+			    bigip_obj.cluster.add_peer( name=devices[i + 1], 
+                                                        mgmt_ip_address=devices_mgmt_ips[i + 1], 
+                                                        username=username, 
+                                                        password=password
+                                                      )
 
 		if not device_group_exists:
-
 		    # Create Sync-Failover Group. 
-		    device_group_created = bigip_obj.Management.DeviceGroup.create(  
-                                                                device_groups = [device_group_name],
-                                                                types = ["DGT_FAILOVER"]
-                                                                )
-
-		    # Due to, Bug alias 479071 TG initial-placement influenced by sync-failover DG w/auto-sync enabled 
-                    # either need to disaable auto-sync when first creating sync-failover-group
-                    # or first offline peers before adding them 
-                    bigip_obj.Management.DeviceGroup.set_autosync_enabled_state (
-                                                                device_groups = [device_group_name],
-                                                                states = ["STATE_DISABLED"]
-                                                                )
+		    # Should return True
+		    device_group_created = bigip_obj.cluster.create(device_group_name)
 		    
 		    # Add device Sync-Failover Group. 
-                    add_deivces = bigip_obj.Management.DeviceGroup.add_device(  
-								  device_groups = [device_group_name],
-                                                                  devices = [ devices ]
-                                                                  )
+		    # should return True
+		    add_devices = bigip_obj.cluster.add_devices(device_group_name, devices )
 
-		    # Initiate a Sync Request
-                    sync_request = bigip_obj.System.ConfigSync.synchronize_to_group_v2(
-                                                    group = device_group_name,
-                                                    device = devices[0],
-                                                    force = True
-                                                    )
+		    # should return True
+		    sync_request = bigip_obj.cluster.sync(name=device_group_name, force_now=True)
 
-	            # Can now re-enable auto-sync  
-		    bigip_obj.Management.DeviceGroup.set_autosync_enabled_state (
-                                                                device_groups = [device_group_name],
-                                                                states = ["STATE_ENABLED"]
-                                                                )
-		    
 		    result = { "changed": True, "cluster_created" : True }
 		else:
 		    result = { "changed": False, "msg" : "Cluster already exists" }
 
 	if action == 'traffic_group':
 	
-	    # exists = bigip_obj.cluster.traffic_group_exists( traffic_group_name )
-	    existing_traffic_groups = bigip_obj.Management.TrafficGroup.get_list()
-            for group in existing_traffic_groups:
-                    if group == traffic_group_name:
-                         traffic_group_exists = True
-                         break	
+	    exists = bigip_obj.cluster.traffic_group_exists( traffic_group_name )
 	    
-	    if not traffic_group_exists:
-		# bigip_obj.cluster.create_traffic_group ( traffic_group_name ) 
-                bigip_obj.Management.TrafficGroup.create( traffic_groups = [traffic_group_name] )
+	    if not exists:
+		bigip_obj.cluster.create_traffic_group ( traffic_group_name ) 
+
 
     except Exception, e:
 	module.fail_json(msg="received exception: %s" % e)
  
-    # module.exit_json(changed=changed, content=result)
+    #module.exit_json(changed=changed, content=result)
     module.exit_json(**result)
 
 # import module snippets
