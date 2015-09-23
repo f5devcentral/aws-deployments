4d8a5f28012606f91e8528e564af8f02c4b130f8
Chris Mutzel <c.mutzel@f5.com>
2015-09-23 14:26:48 -0700

    tests for waf

--- test/test_deployments.py
+++ test/test_deployments.py
@@ -1,4 +1,5 @@
 import sys 
+import json
 import pytest
 
 sys.path.append('./src')
@@ -11,12 +12,11 @@ region_for_lifecyle_tests = config['regions'][0]
 deployment_models = config['deployment_models']
 #deployment_models = ['single-standalone']
 
-
-# for each deployment model, we want to test the full lifecycle
-# these are long running, full system tests
-
-# at intermediate stages, we also want to check that we get back
-#  certain content from back from the APIs
+###
+# It is possible to use this test module to run through deployments of each 
+# type, from init to remove.  Not done at current time to due expense of 
+# doing this....literally, it would probably cost $50 to run each time...
+###
 
 model_stages = {
   'single-standalone':
@@ -106,16 +106,48 @@ model_stages = {
 
 # scope=module => this setup function will be run once before 
 #  executing all the test methods in this module
-@pytest.fixture(scope="function", params=deployment_models) # params=config['regions'])
+@pytest.fixture(scope="function", params=deployment_models)
 def model(request):
-
   return request.param
 
-# can we run a deployment through the full lifecycle?
+# run through all the steps for a deployment
 def test_model_lifecycle(model):
   for s in model_stages[model]:
     inputs = get_namespace(**s.get('inputs', {}))
     inputs.cmd = s['stage']
+
+    # reinstantiate EM each time as we are doing in ./bin/f5aws
+    #  all playbooks contexts should run successfully (exit code 0)
+    em = EnvironmentManager(inputs)
+    outputs = getattr(em, inputs.cmd)()
+    
+    # the stage has custom outputs, check these
+    if 'outputs' in s['stage']:
+      pass
+
+    # otherwise assume that we just ran some playbooks
+    #  check that playbooks executed properly
+    assert (len(outputs['playbook_results'].playbooks) > 0 and
+      outputs['playbook_results'].statuscode == 0)
+
+
+
+# run through all the steps for a deployment
+# do this specifically with deploy_waf = true flag
+# TODO: check somehow that asm is actually slotted 
+# to be provisioned?
+def test_model_lifecycle_w_waf(model):
+  for s in model_stages[model]:
+    inputs = get_namespace(**s.get('inputs', {}))
+    inputs.cmd = s['stage']
+
+    # add the deploy_waf=true flag
+    if inputs.cmd == 'init':
+      # we can just append to the json blob...
+      inputs_str = inputs.extra_vars[0]
+      new_inputs_str = inputs_str[:-1] + ', "deploy_waf": "true"}'
+
+      inputs.extra_vars = [new_inputs_str]
 
     # reinstantiate EM each time as we are doing in ./bin/f5aws
     #  all playbooks contexts should run successfully (exit code 0)
