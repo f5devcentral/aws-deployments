02712cc89532e0a3c53cf5afe9803e3ca4149bba
User1 Desktop <smooth.alg@gmail.com>
2015-10-29 01:39:35 -0700

    cleaned up rest_commands.sh a little bit

--- lab/files/rest_commands.sh
+++ lab/files/rest_commands.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 
-#Edit these variables
+# Edit these variables
 MgmtIP=52.23.93.16
 PublicSelfIP=172.16.13.186
 PrivateSelfIP=172.16.12.39
 RESTPassword="XXXXXXXXXX"
-#and run 
+# and run 
 # bash ./rest_commands.sh
 # to print commands
 
-echo -e "\nDisable GUI Setup" 
+echo -e "\n#Disable GUI Setup" 
 #curl -sk -u restadmin:'$RESTPassword' -H Content-type: application/json -X PATCH -d '{"value":"false"}' https://$MgmtIP/mgmt/tm/sys/db/setup.run | python -m json.tool
 echo "curl -sk -u restadmin:'$RESTPassword' -H Content-type: application/json -X PATCH -d '{\"value\":\"false\"}' https://$MgmtIP/mgmt/tm/sys/db/setup.run | python -m json.tool"
 
-echo -e "\nCreate public vlan"
+echo -e "\n#Create public vlan"
 #curl -sk -u restadmin:'$RESTPassword' -H Content-type: application/json -X POST -d '{"name":"public", "interfaces":"1.1"}' https://$MgmtIP/mgmt/tm/net/vlan | python -m json.tool
 echo "curl -sk -u restadmin:'$RESTPassword' -H Content-type: application/json -X POST -d '{\"name\":\"public\", \"interfaces\":\"1.1\"}' https://$MgmtIP/mgmt/tm/net/vlan | python -m json.tool"
 
-echo -e "\nCreate private vlan"
+echo -e "\n#Create private vlan"
 #curl -sk -u restadmin:'$RESTPassword' -H Content-type: application/json -X POST -d '{"name":"private", "interfaces":"1.2"}' https://$MgmtIP/mgmt/tm/net/vlan | python -m json.tool
 echo "curl -sk -u restadmin:'$RESTPassword' -H Content-type: application/json -X POST -d '{\"name\":\"private\", \"interfaces\":\"1.2\"}' https://$MgmtIP/mgmt/tm/net/vlan | python -m json.tool"
 
-echo -e "\nCreate public SelfIP"
+echo -e "\n#Create public SelfIP"
 #curl -sk -u restadmin:'$RESTPassword' -H Content-type: application/json -X POST -d '{"name":"public_self", "address":"172.16.13.186/24", "vlan":"public"}' https://$MgmtIP/mgmt/tm/net/self | python -m json.tool
 echo "curl -sk -u restadmin:'$RESTPassword' -H Content-type: application/json -X POST -d '{\"name\":\"public_self\", \"address\":\"172.16.13.186/24\", \"vlan\":\"public\"}' https://$MgmtIP/mgmt/tm/net/self | python -m json.tool"
 
-echo -e "\nCreate private SelfIP"
+echo -e "\n#Create private SelfIP"
 #curl -sk -u restadmin:'$RESTPassword' -H Content-type: application/json -X POST -d '{"name":"private_self", "address":"172.16.12.39/24", "vlan":"private"}' https://$MgmtIP/mgmt/tm/net/self | python -m json.tool
 echo "curl -sk -u restadmin:'$RESTPassword' -H Content-type: application/json -X POST -d '{\"name\":\"private_self\", \"address\":\"172.16.12.39/24\", \"vlan\":\"private\"}' https://$MgmtIP/mgmt/tm/net/self | python -m json.tool"
 
-echo -e "\nCreate Pool"
+echo -e "\n#Create Pool"
 #curl -sk -u restadmin:'$RESTPassword' -H Content-type: application/json -X POST -d '{ "name": " vs1_pool", "members":[ {"name":"172.16.12.201:80","address":"172.16.12.201"} ], "monitor": "http" }' https://$MgmtIP/mgmt/tm/ltm/pool | python -m json.tool
 echo "curl -sk -u restadmin:'$RESTPassword' -H Content-type: application/json -X POST -d '{\"name\":\"vs1_pool\",\"members\":[{\"name\":\"172.16.12.201:80\",\"address\":\"172.16.12.201\"}],\"monitor\":\"http\"}' https://$MgmtIP/mgmt/tm/ltm/pool | python -m json.tool"
 
-echo -e "\nCreate iApp"
+echo -e "\n#Create iApp (Edit http_generic_payload.json first)"
 #curl -sk -u restadmin:'$RESTPassword' -H Content-type: application/json -X POST -d @http_generic_iApp_payload.json https://$MgmtIP/mgmt/tm/sys/application/service | python -m json.tool
 echo "curl -sk -u restadmin:'$RESTPassword' -H Content-type: application/json -X POST -d @http_generic_iApp_payload.json https://$MgmtIP/mgmt/tm/sys/application/service | python -m json.tool"
 
